ef7aa90ad1ab9f11c67a19a30bf165039ef29abd
jotform <jotform@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-30 17:22:11 +0000

    tagging version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/jotform-feedback-button/tags/1.0@720485 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ jotform-wp-feedback.js
@@ -0,0 +1,36 @@
+/*
+    Plugin Name: JotForm Wordpress Feedback Plugin
+    Plugin URI: http://www.jotform.com/labs/wordpress
+    Description: JotForm's Wordpress Feedback Plugin
+    Version: 0.1
+    Author: Ertugrul Emre Ertekin
+    Author URI: http://ee.ertek.in
+    License: MIT
+*/
+
+jQuery(document).ready(function($){
+    $.getScript("http://js.jotform.com/JotFormFormPicker.js", function(data, textStatus, jqxhr) {
+        $('#pickForm').on("click",function() {
+            event.preventDefault();
+            var jotformFormPicker = new JotFormFormPicker();
+            jotformFormPicker.openWizard(setFormID);
+        });
+    });
+
+    $('.jotform-color-field').wpColorPicker();
+
+    if(jQuery("#lightBoxType").val() != "") {
+        jQuery("[data-value='"+jQuery("#lightBoxType").val()+"']").parent().addClass("selectedStyle");
+    }
+
+    $('.styleBrowser').on("click",function(){
+        $(".selectedStyle").removeClass("selectedStyle");
+        $(this).addClass("selectedStyle");
+        jQuery("#lightBoxType").val($(this).find("div").attr("data-value"));
+    });
+
+    function setFormID(formID){
+        $("#formID").val(formID);
+    }
+});
+
