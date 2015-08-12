2428cffcea77a5f4df1e3a4619cd49687eb76cfc
eeertekin <eeertekin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-12 12:27:33 +0000

    Tested upto WordPress 4.2.4
    git-svn-id: https://plugins.svn.wordpress.org/jotform-feedback-button/tags/1.0.2@1219114 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ jotform-wp-feedback.js
@@ -0,0 +1,36 @@
+/*
+    Plugin Name: JotForm Feedback Button
+    Plugin URI: http://www.jotform.com/labs/wordpress
+    Description: Display a beautiful feedback button on the side of your blog. When a reader clicks on it a feedback form pops up. Completely customizable.
+    Version: 1.0.2
+    Author: JotForm.com
+    Author URI: http://www.jotform.com
+    License: MIT
+*/
+
+jQuery(document).ready(function($){
+    $.getScript("//js.jotform.com/JotFormFormPicker.js", function(data, textStatus, jqxhr) {
+        $('#pickForm').on("click",function() {
+            event.preventDefault();
+            var jotformFormPicker = new JotFormFormPicker();
+            jotformFormPicker.openWizard(setFormID);
+        });
+    });
+
+    $('.jotform-color-field').wpColorPicker();
+
+    if(jQuery("#lightBoxType").val() != "") {
+        jQuery("[data-value='"+jQuery("#lightBoxType").val()+"']").parent().addClass("selectedStyle");
+    }
+
+    $('.styleBrowser').on("click",function(){
+        $(".selectedStyle").removeClass("selectedStyle");
+        $(this).addClass("selectedStyle");
+        jQuery("#lightBoxType").val($(this).find("div").attr("data-value"));
+    });
+
+    function setFormID(formID){
+        $("#formID").val(formID);
+    }
+});
+
---
+++ jotform-wp-feedback.php
@@ -0,0 +1,87 @@
+<?php
+/*
+    Plugin Name: JotForm Feedback Button
+    Plugin URI: http://www.jotform.com/labs/wordpress
+    Description: Display a beautiful feedback button on the side of your blog. When a reader clicks on it a feedback form pops up. Completely customizable.
+    Version: 1.0.2
+    Author: JotForm.com
+    Author URI: http://www.jotform.com
+    License: MIT
+*/
+
+class JotFormWPFeedback {
+
+    public function __construct() {
+        add_action( 'admin_menu',   array($this,'addAdminMenu') );
+        add_action( 'admin_enqueue_scripts', array($this,'mw_enqueue_form_picker') );
+        add_action( 'admin_init',   array($this, 'register_button_options') );
+        add_action( 'wp_footer',    array($this, 'generateFeedBackCode') );
+        add_action( 'wp_enqueue_scripts', array($this, 'loadJotFormFeedBack') );
+    }
+
+    public function loadJotFormFeedBack() {
+        wp_enqueue_script(
+            'jotform-feedback-button',
+            '//www.jotform.com/static/feedbackWP.js',
+            array( 'jquery','jquery-ui-core', 'jquery-ui-widget','jquery-ui-mouse','jquery-ui-draggable' )
+        );
+    }
+
+    public function register_button_options() {
+            register_setting( 'jotform-wp-feedback-options', 'buttonOptions' );
+    }
+
+    public function mw_enqueue_form_picker( $hook_suffix ) {
+        if($hook_suffix == "settings_page_jotform-feedback") {
+            wp_enqueue_style( 'wp-color-picker' );
+            wp_enqueue_script( 'jotform-wp-feedback-js', plugins_url('jotform-wp-feedback.js', __FILE__ ), array( 'wp-color-picker' ), false, true );
+        }
+    }
+
+    public function addAdminMenu() {
+        add_options_page( 'JotForm Feedback Button', 'JotForm Feedback Button', 'manage_options', 'jotform-feedback', array($this,'showOptions') );
+    }
+
+    public function showOptions() {
+        if ( !current_user_can( 'manage_options' ) )  {
+            wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+        }
+
+        $options = get_option('buttonOptions');
+        if(!$options) {
+            $options = array("formTitle" => "Feedback", 
+                "buttonColor" => "#F59202", 
+                "labelColor" => "#FFFFFF", 
+                "screenAlignment" => "bottom", 
+                "horizontalAlignment" => "right", 
+                "lightBoxType" => "false",
+                "formWidth" => 700,
+                "formHeight" => 500
+                );
+        }
+        include plugin_dir_path(__FILE__) . "jotform-wp-feedback-options.php";
+    }
+
+    public function generateFeedBackCode() {
+
+            $options = get_option('buttonOptions');
+            echo '<script type="text/javascript">
+                      new JotformFeedback({
+                         formId     : "'.   $options["formID"].'",
+                         buttonText : "'.   $options["formTitle"] .'",
+                         base       : "http://www.jotform.com/",
+                         background : "'.   $options["buttonColor"].'",
+                         fontColor  : "'.   $options["labelColor"] .'",
+                         buttonSide : "'.   $options["screenAlignment"] .'",
+                         buttonAlign: "'.   $options["horizontalAlignment"] .'",
+                         type       : "'.   $options["lightBoxType"] .'",
+                         width      : "'.   $options["formWidth"] .'",
+                         height     : "'.   $options["formHeight"] .'",
+                      });
+                    </script>';
+    }
+}
+
+$jotformwp = new JotFormWPFeedback();
+
+?>
